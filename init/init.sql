3b8f78475bc708c690e57703e4a8be4e444d7bb9
DusanStevic <stevicdule@gmail.com>
2021-09-15 13:59:13 +0200

    db schemas

--- init/init.sql
+++ init/init.sql
@@ -1,7 +1,2 @@
-# create databases
-CREATE DATABASE IF NOT EXISTS `auth_db`;
-CREATE DATABASE IF NOT EXISTS `user_db`;
-
-# create root user and grant rights
-CREATE USER 'sa'@'localhost' IDENTIFIED BY 'zgadija';
-GRANT ALL PRIVILEGES ON *.* TO 'sa'@'%';
+CREATE DATABASE auth_db;
+CREATE DATABASE user_db;
